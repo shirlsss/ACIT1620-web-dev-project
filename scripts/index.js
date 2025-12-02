0e2d551eb8b8ce7fdb39291ac155cb65b3818e5d
shirley zheng <jzheng96@my.bcit.ca>
2025-12-01 18:37:46 -0800

    updated repo with work and files done on desktop

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+let mybutton = document.getElementById("top-button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+var modal = document.getElementById("img-modal");
+
+var img = document.getElementById("img1");
+var imgModal = document.getElementById("img01")
+var captionText = document.getElementById("caption1")
+img.onclick = function() {
+    modal.style.display = "block";
+    imgModal.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
